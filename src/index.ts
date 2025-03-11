6a73985e1846a8a1ab2659b28bb8538a8bf53ff2
liton61 <litonofficial61@gmail.com>
2025-03-11 23:38:17 +0600

    cors-problem-solve

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const app: Application = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://vote-app-client.vercel.app',
+    origin: 'https://vote-app-nu.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], 
   })
