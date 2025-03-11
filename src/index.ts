6f2e8e67eca08bd1475ed5b4645f3961f914bfd6
liton61 <litonofficial61@gmail.com>
2025-03-11 23:32:50 +0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,10 @@ app.use(
   cors({
     origin: 'https://vote-app-client.vercel.app',
     credentials: true,
-  }),
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+  })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
